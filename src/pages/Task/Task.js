e9d245a122d5e90f35ef63e778abfa3590152fbb
Facundo Alemandi <facuinstituto10@gmail.com>
2022-08-15 00:24:10 -0300

    Eliminaando Tasks

--- src/pages/Task/Task.js
+++ src/pages/Task/Task.js
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import TaskImg from "../../components/images/example-8.svg";
 import dateDesktop from "../../components/images/Date.svg";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import { MdOutlineDelete } from "react-icons/md";
 
 const Main = styled.main`
   background-color: #fbf3d8;
   height: auto;
   min-height: 100vh;
+  @media (min-width: 1080px) {
+    display: flex;
+  }
 `;
 const Img = styled.img`
   width: 70vw;
@@ -47,7 +51,7 @@ const DivOne = styled.div`
 
 const Button = styled.button`
   background: linear-gradient(to bottom, #7a5bf5, #b56bff);
-  width: 50vw;
+  width: 40vw;
   max-width: 300px;
   display: flex;
   justify-content: center;
@@ -67,7 +71,16 @@ const Button = styled.button`
   }
 `;
 
-const SectionOne = styled.section``;
+const SectionOne = styled.section`
+  @media (min-width: 1080px) {
+    display: flex;
+    width: 1080px;
+    margin: auto;
+  }
+  @media (min-width: 1380px) {
+    width: 1380px;
+  }
+`;
 
 const Modal = styled.section`
   position: absolute;
@@ -80,9 +93,11 @@ const Modal = styled.section`
 
 const TheForm = styled(Form)`
   width: 90vw;
-  height: 500px;
+  height: auto;
   margin: 100px auto;
   background-color: white;
+  border-radius: 15px;
+  box-shadow: 5px 5px 5px 5px rgba(0, 0, 0, 0.281);
 `;
 
 const Input = styled(Field)`
@@ -91,8 +106,21 @@ const Input = styled(Field)`
   width: 90%;
   padding: 10px;
   border: none;
+  border-radius: 10px;
+  border: 1px solid rgba(175, 175, 175, 0.473);
+`;
+
+const Textarea = styled(Field)`
+  display: flex;
+  margin-top: 5px;
+  width: 90%;
+  padding: 10px;
+  border: none;
   border-radius: 5px;
   border: 1px solid grey;
+  resize: none;
+  border-radius: 10px;
+  border: 1px solid rgba(175, 175, 175, 0.473);
 `;
 
 const DivForm = styled.div`
@@ -111,15 +139,81 @@ const Error = styled.p`
 `;
 
 const Labels = styled.label`
- margin-top: 10px;
- font-family: 'Roboto', sans-serif;
- font-size: 16px;
+  margin-top: 10px;
+  font-family: "Montserrat", sans-serif;
+  font-size: 16px;
+`;
 
-`
+const SectionBtns = styled.section`
+  display: flex;
+`;
+
+const ContainerTask = styled.section`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  width: 90vw;
+  max-width: 900px;
+  margin: auto;
+  @media (min-width: 1080px) {
+    margin: 0;
+  }
+`;
+
+const SectionTask = styled.section`
+  list-style: none;
+  padding: 5px;
+  margin: 5px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 2px 2px 5px 2px rgba(0, 0, 0, 0.199);
+
+  @media (min-width: 1080px) {
+    height: max-content;
+  }
+`;
+
 const Task = () => {
   const [modal, setModal] = useState(false);
-
   const openModal = () => (!modal ? setModal(true) : setModal(false));
+
+  function generateUUID() {
+    var d = new Date().getTime();
+    var uuid = "xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx".replace(
+      /[xy]/g,
+      function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === "x" ? r : r && 0x3 | 0x8).toString(16);
+      }
+    );
+    return uuid;
+  }
+
+  const localTask = localStorage.getItem("task");
+
+  let parseTask;
+
+  if (!localTask) {
+    localStorage.setItem("task", JSON.stringify([]));
+    parseTask = [];
+  } else {
+    parseTask = JSON.parse(localTask);
+  }
+  const [tasks, setTasks] = useState(parseTask);
+
+  const saveTasks = (newTasks) => {
+      const taskString = JSON.stringify(newTasks);
+      localStorage.setItem('task', taskString);
+      setTasks(newTasks)
+  }
+
+  const deleteTask = (task) => {
+     const taskIndex = tasks.findIndex(el => el.id === task.id);
+     console.log(taskIndex);
+     const newTasks = [...tasks];
+     newTasks.splice(taskIndex, 1);
+     saveTasks(newTasks)
+  };
 
   return (
     <>
@@ -133,9 +227,18 @@ const Task = () => {
 
           <Modal value={modal}>
             <Formik
-              initialValues={{ name: "" }}
-              onSubmit={(valores) => {
-                console.log(valores);
+              initialValues={{ name: "", description: "" }}
+              onSubmit={(valores, { resetForm }) => {
+                if (valores.name && valores.description) {
+                  setTasks([...tasks, valores]);
+                  valores.id = generateUUID();
+                  localStorage.setItem(
+                    "task",
+                    JSON.stringify([...tasks, valores])
+                  );
+                  resetForm();
+                  return openModal();
+                }
               }}
               validate={(valores) => {
                 let errors = {};
@@ -146,25 +249,91 @@ const Task = () => {
                   errors.name = "El nombre no puede contener números";
                 }
 
+                if (!valores.description) {
+                  errors.description = "Por favor, ingresa una descripción";
+                } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(errors.description)) {
+                  errors.description = "L";
+                }
+
                 return errors;
               }}
             >
               {({ errors }) => (
                 <TheForm>
                   <DivForm>
-                    <Labels htmlFor="name">Nombre</Labels>
+                    <Labels htmlFor="name">Tarea</Labels>
                     <Input name="name" />
-                    <ErrorMessage name="name" component={() => <Error>{errors.name}</Error>}/>
+                    <ErrorMessage
+                      name="name"
+                      component={() => <Error>{errors.name}</Error>}
+                    />
                   </DivForm>
 
-                  <Button type="button" onClick={openModal}>
-                    Cerrar
-                  </Button>
-                  <Button type="submit">Agregar</Button>
+                  <DivForm>
+                    <Labels htmlFor="description">Descripción</Labels>
+                    <Textarea
+                      name="description"
+                      type="textarea"
+                      component={"textarea"}
+                    />
+                    <ErrorMessage
+                      name="description"
+                      component={() => <Error>{errors.description}</Error>}
+                    />
+                  </DivForm>
+
+                  <DivForm>
+                    <Labels htmlFor="dia">Més</Labels>
+
+                    <Input name="month" component="select">
+                      <option value="Enero">Enero</option>
+                      <option value="Febrero">Febrero</option>
+                      <option value="Marzo">Marzo</option>
+                      <option value="Abril">Abril</option>
+                      <option value="Mayo">Mayo</option>
+                      <option value="Junio">Junio</option>
+                      <option value="Julio">Julio</option>
+                      <option value="Agosto">Agosto</option>
+                      <option value="Septiembre">Septiembre</option>
+                      <option value="Octubre">Octubre</option>
+                      <option value="Noviembre">Noviembre </option>
+                      <option value="Diciembre">Diciembre</option>
+                    </Input>
+
+                    <ErrorMessage
+                      name="dia"
+                      component={() => <Error>{errors.name}</Error>}
+                    />
+                  </DivForm>
+
+                  <SectionBtns>
+                    <Button type="button" onClick={openModal}>
+                      Cerrar
+                    </Button>
+                    <Button type="submit">Agregar</Button>
+                  </SectionBtns>
                 </TheForm>
               )}
             </Formik>
           </Modal>
+
+          {tasks.length === 0 ? (
+            <p>Facu</p>
+          ) : (
+            <ContainerTask>
+              {tasks.length >= 1 &&
+                tasks.map((el) => (
+                  <SectionTask key={el.id}>
+                    <div>
+                      <h2>{el.name}</h2>
+                      <MdOutlineDelete onClick={() => deleteTask(el)} />
+                    </div>
+                    <p>{el.description}</p>
+                    <span>{el.month}</span>
+                  </SectionTask>
+                ))}
+            </ContainerTask>
+          )}
         </SectionOne>
       </Main>
     </>
