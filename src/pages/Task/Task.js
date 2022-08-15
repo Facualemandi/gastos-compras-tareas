0d32d39eeb79b9ed2202f6d3b4541fc49dbd5f89
Facundo Alemandi <facuinstituto10@gmail.com>
2022-08-15 02:12:43 -0300

    Segunda

--- src/pages/Task/Task.js
+++ src/pages/Task/Task.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import calendar from "../../components/images/calendar.svg";
+import { Form, Field, ErrorMessage } from "formik";
 
 const Main = styled.main`
   width: 100vw;
@@ -74,8 +75,59 @@ const SectionModal = styled.section`
   flex-direction: column;
   margin: auto;
 `;
-///////////////////////////////////////////
+const TheForm = styled(Form)`
+  width: 90vw;
+  height: auto;
+  margin: 100px auto;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 5px 5px 5px 5px rgba(0, 0, 0, 0.281);
+`;
 
+const Input = styled(Field)`
+  display: flex;
+  margin-top: 5px;
+  width: 90%;
+  padding: 10px;
+  border: none;
+  border-radius: 10px;
+  border: 1px solid rgba(175, 175, 175, 0.473);
+`;
+
+const Textarea = styled(Field)`
+  display: flex;
+  margin-top: 5px;
+  width: 90%;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  border: 1px solid grey;
+  resize: none;
+  border-radius: 10px;
+  border: 1px solid rgba(175, 175, 175, 0.473);
+`;
+
+const DivForm = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  margin-top: 10px;
+`;
+
+const Error = styled.p`
+  font-size: 14px;
+  font-family: "Roboto", sans-serif;
+  color: red;
+  margin-top: 5px;
+`;
+
+const Labels = styled.label`
+  margin-top: 10px;
+  font-family: "Montserrat", sans-serif;
+  font-size: 16px;
+`;
+///////////////////////////////////////////
 
 const SectionTwo = styled.section`
   @media (min-width: 1080px) {
@@ -100,9 +152,29 @@ const Task = () => {
 
         <Modal value={modal}>
           <SectionModal>
+            <TheForm >
+              <DivForm>
+                <Labels htmlFor="name">Tarea</Labels>
+                <Input name="name" />
+                <ErrorMessage
+                  name="name"
+                  component={() => <Error>{errors.name}</Error>}
+                />
+              </DivForm>
 
-             <input/>
-             <input/>
+              <DivForm>
+                <Labels htmlFor="description">Descripción</Labels>
+                <Textarea
+                  name="description"
+                  type="textarea"
+                  component={"textarea"}
+                />
+                <ErrorMessage
+                  name="description"
+                  component={() => <Error>{errors.description}</Error>}
+                />
+              </DivForm>
+            </TheForm>
             <ButtonAdd onClick={theModal}>Cerrar</ButtonAdd>
           </SectionModal>
         </Modal>
